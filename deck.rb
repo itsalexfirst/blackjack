e3c34fc6af9edac8978ca660061bb9bafe5713ca
itsalexfirst <itsalex@yandex.ru>
2020-02-23 19:55:12 +0300

    deck

--- deck.rb
+++ deck.rb
@@ -1,3 +1,16 @@
 class Deck
+  attr_reader :cards
 
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def take_card
+    @cards.last unless @cards.empty?
+    @cards.pop
+  end
+
+  def shuffle
+    @cards.shuffle!
+  end
 end
