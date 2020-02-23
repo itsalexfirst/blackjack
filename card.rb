e2e77f5938d86c256c1bf0e2e35db005e6424bc3
itsalexfirst <itsalex@yandex.ru>
2020-02-23 20:00:18 +0300

    cards with ranks and suits constants

--- card.rb
+++ card.rb
@@ -1,3 +1,13 @@
 class Card
+  VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+  SUITS = ['♣', '♦', '♥', '♠']
 
+  def initialize(value, suit)
+    @value = value
+    @suit = suit
+  end
+
+  def to_s
+    "#{@value}#{@suit}"
+  end
 end
