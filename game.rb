de6ead753382081e866fdbad29b97adb1e66d5b4
itsalexfirst <itsalex@yandex.ru>
2020-02-27 18:05:59 +0300

    all features

--- game.rb
+++ game.rb
@@ -2,34 +2,82 @@ class Game
   def initialize
     @deck = Deck.new(Card.all)
     @dealer = Dealer.new
-    @round_run = true
   end
 
   def start
     @player = ask_name
     @table = Table.new(@deck, @player, @dealer)
 
+    new_round
+  end
 
+  def new_round
+    @round_run = true
     @table.deals
 
-    round
+    loop do
+      break unless @round_run
+      break if all_have_three
+      round
+    end
 
-    #score
+    round_end
 
-    play_again
+    new_round if @table.can_start_game? && play_again?
+
+    game_over
   end
 
   def round
-    show_card(@player)
-    show_score(@player)
+    show_card_and_score(@player)
 
-    show_card(@dealer)
-    show_score(@dealer)
+    show_card_and_score(@dealer)
 
-    #player_turn
-    #dealer_turn
+    player_turn(@player) if @round_run
+    dealer_turn(@dealer) if @round_run
   end
 
+  def round_end
+    show_card_and_score(@player)
+    show_card_and_score(@dealer)
+    if score(@player) == score(@dealer)
+      draw
+    else
+      win
+    end
+  end
+
+  def show_card_and_score(player)
+    show_card(player)
+    show_score(player)
+  end
+
+  def draw
+    @table.draw
+  end
+
+  def all_have_three
+    if @table.can_give_card?(@dealer) || @table.can_give_card?(@player)
+      return false
+    else
+      @round_run = false
+      return true
+    end
+  end
+
+  def win
+    if score(@player) < 21 && score(@player) > score(@dealer)
+      winner = @player
+      looser = @dealer
+    else
+      winner = @dealer
+      looser = @player
+    end
+
+    @table.winner(winner)
+    @table.looser(looser)
+    puts "#{winner.name} win."
+  end
 
   def score(player)
     @table.score(player)
@@ -52,11 +100,35 @@ class Game
     end
   end
 
-  def player_turn
-    puts '1. Skip 2. Take card 3. Show card'
+  def player_turn(player)
+    puts '1 - Skip turn; 2 - Take card; 3 - Show card'
+    choice = gets.chomp
+      case choice
+      when '1'
+        return
+      when '2'
+        @table.can_give_card?(player) ? @table.give_card(player) : @round_run = false
+      when '3'
+        @round_run = false
+      else
+        player_turn(player)
+      end
+    end
+  end
+
+  def dealer_turn(player)
+    @table.give_card(player) if @table.can_give_card?(player) && @table.score(player) < 17
   end
 
   def play_again
+    puts "#{@player.name} balance: #{@player.balance}"
+    puts 'Play again? y/n'
+    gets.chomp
+  end
+
+  def play_again?
+    return unless play_again == 'y'
+    true
   end
 
   def ask_name
@@ -64,4 +136,8 @@ class Game
     name = gets.chomp
     return Player.new(name)
   end
+
+  def game_over
+    puts 'Game over'
+    puts "#{@player.name} balance: #{@player.balance}. Dealer balance: #{@dealer.balance}"
 end
